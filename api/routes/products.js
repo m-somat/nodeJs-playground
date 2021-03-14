fb6b17c75e6284beb4068ee5c5712e93944beed1
m-somat <mostafasomat1@gmail.com>
2021-03-15 01:18:25 +0200

    products finished

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,3 +1,8 @@
+// 200: (Accessed) The request has succeeded
+// 201: (Created) the request has succeeded and has led to the creation of a resource
+// 404: (Not found) the server can't find the requested resource
+// 500: (Wrong resource) the server encountered an unexpected condition
+
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -8,10 +13,21 @@ const Product = require('../models/product');
 router.get('/', (req,res,next) => {
 	Product
 	  .find()
+	  .select("name price _id")
 	  .exec()
 	  .then(docs => {
-	  	console.log(docs);
-	  	res.status(200).json(docs);
+	  	const response = {
+	  		"count": docs.length,
+	  		"products":  docs.map(doc => {
+	  			return {
+	  				"name": doc.name,
+	  				"price": doc.price,
+	  				"id": doc._id,
+	  				"url": 'http://localhost:3000/products/' + doc._id
+	  			}
+	  		})
+	  	}
+	  	res.status(200).json(response);
 	  })
 	  .catch(err => {
 	  	console.log(err);
@@ -31,10 +47,14 @@ router.post('/', (req,res,next) => {
 	product
 	  .save()
 	  .then(result => {
-		console.log(result);
 		res.status(201).json({
-			"msg": 'products sent',
-			"product": result
+			"msg": 'Created product successfully',
+			"product": {
+				"name": result.name,
+				"price": result.price,
+				"id": result._id,
+				"url": 'http://localhost:3000/products/' + result._id
+			}
 		})
 	  })
 	  .catch(err => {
@@ -50,17 +70,19 @@ router.get('/:productId', (req,res,next) => {
 
 	Product
 	  .findById(id)
+	  .select("name price _id")
 	  .exec()
 	  .then(doc => {
-	  	console.log("From Database", doc);
 	  	if (doc){
-	  		res.status(200).json(doc);
+	  		res.status(200).json({
+	  			"product": doc,
+	  			"refererUrl": "go back: http://localhost:3000/products"
+	  		});
 	  	} else{
 	  		res.status(404).json({"msg": "product doesn't exist"})
 	  	}
 	  })
 	  .catch(err => {
-	  	console.log(err);
 	  	res.status(500).json({"error":err});
 	  });
 });
@@ -72,10 +94,12 @@ router.delete('/:productId', (req,res,next) => {
 	  .findOneAndDelete({"_id": id})
 	  .exec()
 	  .then(result => {
-	  	res.status(200).json("Deleted product: " + result._id);
+	  	res.status(200).json({
+	  		"msg": "Deleted product: " + id,
+	  		"refererUrl": "go back: http://localhost:3000/products"
+	  	});
 	  })
 	  .catch(err => {
-	  	console.log(err);
 	  	res.status(500).json({
 	  		"error": err
 	  	})
@@ -85,7 +109,7 @@ router.delete('/:productId', (req,res,next) => {
 router.patch('/:productId', (req,res,next) => {
 	const id = req.params.productId;
 
-	// update the changed key only
+	// update the changed property only
 	const updateOps = {};
 	for(const ops of req.body) {
 		updateOps[ops.propName] = ops.value;
@@ -95,11 +119,13 @@ router.patch('/:productId', (req,res,next) => {
 	  .update({"_id": id}, {"$set": updateOps})
 	  .exec()
 	  .then(result => {
-	  	console.log(result);
-	  	res.status(200).json(result);
+  		res.status(200).json({
+  			"msg": "product updated",
+  			"product": result,
+  			"url": "http://localhost:3000/products/" + id
+  		});
 	  })
 	  .catch(err => {
-	  	console.log(err);
 	  	res.status(500).json({
 	  		"error": err
 	  	});
